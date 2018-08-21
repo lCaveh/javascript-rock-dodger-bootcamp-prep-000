a18edb603b0844daf18335ce6c7f216003e7fbcd
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-21 22:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,51 +16,23 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
     const dodgerRightEdge = positionToInteger(DODGER.style.left)+ 40;
-
-    const rockLeftEdge = positionToInteger(rock.style.left)
-
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+    const rockLeftEdge = positionToInteger(rock.style.left);
     const rockRightEdge = positionToInteger(rock.style.left)+ 20;
-
     if (((rockLeftEdge<=dodgerLeftEdge)&&(rockRightEdge>=dodgerLeftEdge)) || ((rockLeftEdge>=dodgerLeftEdge)&& (rockRightEdge<=dodgerRightEdge)) || ((rockLeftEdge<=dodgerRightEdge)&&(rockRightEdge>=dodgerRightEdge))
-    /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge */
-               ) {
+      ) {
       return true;
     }
   }
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
-
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
-
-  // Hmmm, why would we have used `var` here?
-  var top = 0
-
-  rock.style.top = top
+  const rock = document.createElement('div');
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
+  var top = 0;
+  rock.style.top = top;
   GAME.appendChild(rock);
   
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
 
   top= positionToInteger(rock.style.top,10);
   function moveRock() {
