62ca5d36f027b94338411ef424fbfc2f1a3eb408
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-21 22:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,7 +90,6 @@ function positionToInteger(p) {
 function start() {
   window.addEventListener('keydown', moveDodger);
   START.style.display = 'none';
-
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
   }, 1000);
